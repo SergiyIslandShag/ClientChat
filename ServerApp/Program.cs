95c2e44fb9be10ca3cabbc09cff4c503a728fea4
sergiykoziar13@gmail.com <sergiykoziar13@gmail.com>
2024-10-21 20:32:55 +0300

    TcpProtocol

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -7,46 +7,27 @@ namespace ServerApp
     class ServerChat
     {
         const short port = 4040;
-        const string JOIN_CMD = "$<join>";
-        UdpClient server;
-        List<IPEndPoint> members;
-        IPEndPoint clientEndPoint = null;
+        const string address = "127.0.0.1";
+
+        TcpListener listener = null;
         public ServerChat()
         {
-            server = new UdpClient(port);
-            members = new List<IPEndPoint>();
-        }
-        private void AddMember()
-        {
-            members.Add(clientEndPoint);
-            Console.WriteLine("Member was added!");
-        }
-        private void SendToAll(byte[]data)
-        {
-            foreach (var member in members)
-            {
-                server.SendAsync(data, data.Length, member);
-            }
-        }
+            listener = new TcpListener(new IPEndPoint(IPAddress.Parse(address), port));
+        }      
         public void Start()
-        {             
+        {
+            listener.Start();
+            Console.WriteLine("Waiting for connection.....");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Connected!!!!");
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
             while (true)
             {
-                byte[] data = server.Receive(ref clientEndPoint);
-                string message = Encoding.UTF8.GetString(data);
-                Console.WriteLine($"{message} from {clientEndPoint}. Date: {DateTime.Now}");
-                switch (message)
-                {
-                    case JOIN_CMD:
-                        AddMember();
-                        break;
-                    default:
-                        SendToAll(data);
-                        break;
-                }
+                string message = reader.ReadLine();
+                Console.WriteLine($"{message} from {client.Client.LocalEndPoint}. Date: {DateTime.Now}");
             }
         }
-
     }
     internal class Program
     {
